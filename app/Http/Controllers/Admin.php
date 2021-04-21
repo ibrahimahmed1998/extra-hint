9c60230d35db6e68135a78a677703b003307497b
ibrahim ahmed <hema.1998.man@gmail.com>
2021-04-21 22:33:06 +0200

    21-4-2021

--- app/Http/Controllers/Admin.php
+++ app/Http/Controllers/Admin.php
@@ -14,22 +14,24 @@ class Admin extends Controller
     public function search(Request $request) // by name 
     {
         $user = User::where('full_name', $request->name)->get();
-        return response()->json(['message' =>  $user], 201);
+        return response()->json(['success' =>  $user], 201);
     }
 
     public function list_all() // list all users 
     {
         $user = User::all();
-        return response()->json(['message' =>  $user], 201);
+        return response()->json(['success' =>  $user], 201);
     }
 
     public function delete_user(validate_delete  $request)
     {
-        if (User::find($request->id)) {
+        if (User::find($request->id)) 
+        {
             User::find($request->id)->delete();
-            return response()->json(['message' => 'User Sucessfully Deleted  '], 201);
-        } else {
-            return response()->json(['message' => 'User not found '], 201);
+            return response()->json(['success' => 'User  Deleted  '], 201);
+        } else 
+        {
+            return response()->json(['error' => 'User not found '], 201);
         }
     }
 
