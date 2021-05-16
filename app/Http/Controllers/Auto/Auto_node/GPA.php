d745d2a929ee62d51a8f7e4195d527f64d05c558
ibrahim ahmed <hema.1998.man@gmail.com>
2021-05-16 12:33:49 +0200

    nice

--- app/Http/Controllers/Auto/Auto_node/GPA.php
+++ app/Http/Controllers/Auto/Auto_node/GPA.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers\Auto\Auto_node;
 
 use App\Http\Controllers\Auto\Auto_degree;
@@ -8,194 +9,188 @@ use App\Models\enroll;
 
 class GPA extends Controller
 {
-public function __construct() { $this->middleware('auth:api', ['except' =>  []]); } 
-
-public function gpa($id,$msg,$year,$semester) // $msg = sgpa || cgpa
-{
-    $points = 0.0;        $Qulaity = 0.0;            $total_quality=0;       $total_cch=0;      $code = "";
-    $form = [];
-    
-    $auto = new Auto_degree(); $auto->auto_degree();
-
-    $test=enroll::where('Student_id',$id)->first();     
-    if(!$test) {return 0;}     // STUDENT NOT ENROLLED COURSES 
-
-    else
+    public function __construct()
     {
-        $c = enroll::where('Student_id',$id)->get(); 
+        $this->middleware('auth:api', ['except' =>  []]);
+    }
 
-        if($msg=="sgpa")
-        {
-            $y=enroll::where('year',$year)->first();
-            $s=enroll::where('semester',$semester)->first();
+    public function gpa($id, $msg, $year, $semester) // $msg = sgpa || cgpa
+    {
+        $points = 0.0;
+        $Qulaity = 0.0;
+        $total_quality = 0;
+        $total_cch = 0;
+        $code = "";
+        $form = [];
+        $aaa = [];
+        $bbb  = [];
+        $ccc  = [];
+
+        $auto = new Auto_degree();
+        $auto->auto_degree();
+
+        $test = enroll::where('Student_id', $id)->first();
+        if (!$test) {
+            return 0;
+        }     // STUDENT NOT ENROLLED COURSES 
+
+        else {
+            $c = enroll::where('Student_id', $id)->get();
+
+            if ($msg == "sgpa") {
+                $y = enroll::where('year', $year)->first();
+                $s = enroll::where('semester', $semester)->first();
 
 
-        if(!$y || !$s) 
-         {
-            return response()->json(['err' => "not Enrolled in: ".$year." or this semester"], 401); 
-         }
+                if (!$y || !$s) {
+                    return response()->json(['err' => "not Enrolled in: " . $year . " or this semester"], 401);
+                }
 
-         $test_sem =enroll::where('Student_id', $id)->where('semester',$s)->where('year',$y)->first(); 
+                $test_sem = enroll::where('Student_id', $id)->where('semester', $s)->where('year', $y)->first();
 
-         if(!$test_sem){ return response()->json(['err' => "not Enrolled Courses in this semester"], 401); }
+                if (!$test_sem) {
+                    return response()->json(['err' => "not Enrolled Courses in this semester"], 401);
+                }
 
-            $c=enroll::where('Student_id', $id)->where('semester',$semester)->where('year',$year)->get();  
-
-        }
-        for ($i=0; $i <$c->count() ; $i++)   { $arr[] = $c[$i]; }
-            
-        for ($i=0; $i<$c->count() ;  $i++) 
-        {
-            $cd = Course::where('ccode', $arr[$i]->ccode)->value('dtotal'); // Course Degree
-        
-            $cch = Course::where('ccode', $arr[$i]->ccode)->value('cch'); // Course Credit Hours
-        
-            $sd = $arr[$i]->htotal_d;        // Student Degree
-
-            if ($sd >= $cd * 0.90) 
-            {
-                $code = 'A';
-                $points = 4.0;
-                $Qulaity = $points * $cch;
-            } else if ($sd >= $cd * 0.85 && $sd <  $cd * 0.90) {
-                $code = 'A-';
-                $points = 3.67;
-                $Qulaity = $points * $cch;
-            } else if ($sd >= $cd * 0.80 && $sd <  $cd * 0.85) {
-                $code = 'B+';
-                $points = 3.33;
-                $Qulaity = $points * $cch;
-            } else if ($sd >= $cd * 0.75 && $sd <  $cd * 0.80) {
-                $code = 'B';
-                $points = 3.00;
-                $Qulaity = $points * $cch;
-            } else if ($sd >= $cd * 0.70 && $sd <  $cd * 0.75) {
-                $code = 'C+';
-                $points = 2.67;
-                $Qulaity = $points * $cch;
-            } else if ($sd >= $cd * 0.65 && $sd <  $cd * 0.70) {
-                $code = 'C';
-                $points = 2.33;
-                $Qulaity = $points * $cch;
-            } else if ($sd >= $cd * 0.60 && $sd <  $cd * 0.65) {
-                $code = 'D';
-                $points = 2.0;
-                $Qulaity = $points * $cch;
-            } else      if ($sd < $cd * 0.60) 
-            {
-                $code = 'F';
-                $points = 0.0;
-                $Qulaity = $points * $cch;
+                $c = enroll::where('Student_id', $id)->where('semester', $semester)->where('year', $year)->get();
             }
 
-            $total_quality = $total_quality + $Qulaity;
-            $total_cch = $total_cch + $cch;
-            
-        //dd($c->count());
+            for ($i = 0; $i < $c->count(); $i++) {
+                $arr[] = $c[$i];
+            }
 
-        
-        $form[] =
-        [
-            "Course code=" => $c[$i]->ccode,
-            "Grade=" => $code,
-            "Points=" => $points,
-            "degree" => $sd,
-            "hpass" => $c[$i]->hpass,
-            "semester" =>$c[$i]->semester,
-            "year" => $c[$i]->year,
-            "******" => "******",   ////////////////////////////////////////////////
-            'hmedterm_d'=> $c[$i]->year, 
-            'hlab_d'=> $c[$i]->hlab_d,
-            'horal_d'=> $c[$i]->horal_d,
-            'hclass_work_d'=> $c[$i]->hclass_work_d,
-            'hfinal_d'=> $c[$i]->hfinal_d
-        ];
+            for ($i = 0; $i < $c->count(); $i++) 
+            {
+                $cd = Course::where('ccode', $arr[$i]->ccode)->value('dtotal'); // Course Degree
 
-            for ($i=0; $i <$c->count() ; $i++) 
-            { 
-                if($c[$i]->semester == 1)
-                {
-                    $aaa []= 
+                $cch = Course::where('ccode', $arr[$i]->ccode)->value('cch'); // Course Credit Hours
+
+                $sd = $arr[$i]->htotal_d;        // Student Degree
+
+                if ($sd >= $cd * 0.90) {
+                    $code = 'A';
+                    $points = 4.0;
+                    $Qulaity = $points * $cch;
+                } else if ($sd >= $cd * 0.85 && $sd <  $cd * 0.90) {
+                    $code = 'A-';
+                    $points = 3.67;
+                    $Qulaity = $points * $cch;
+                } else if ($sd >= $cd * 0.80 && $sd <  $cd * 0.85) {
+                    $code = 'B+';
+                    $points = 3.33;
+                    $Qulaity = $points * $cch;
+                } else if ($sd >= $cd * 0.75 && $sd <  $cd * 0.80) {
+                    $code = 'B';
+                    $points = 3.00;
+                    $Qulaity = $points * $cch;
+                } else if ($sd >= $cd * 0.70 && $sd <  $cd * 0.75) {
+                    $code = 'C+';
+                    $points = 2.67;
+                    $Qulaity = $points * $cch;
+                } else if ($sd >= $cd * 0.65 && $sd <  $cd * 0.70) {
+                    $code = 'C';
+                    $points = 2.33;
+                    $Qulaity = $points * $cch;
+                } else if ($sd >= $cd * 0.60 && $sd <  $cd * 0.65) {
+                    $code = 'D';
+                    $points = 2.0;
+                    $Qulaity = $points * $cch;
+                } else if ($sd < $cd * 0.60) {
+                    $code = 'F';
+                    $points = 0.0;
+                    $Qulaity = $points * $cch;
+                }
+
+                $total_quality = $total_quality + $Qulaity;
+                $total_cch = $total_cch + $cch;
+
+                $form[] =
                     [
-                        "semester"=>$c[$i]->semester,
-                        "year"=> $c[$i]->year,
-                        "ccode"=>$c[$i]->ccode,
-                        "hmedterm_d"=> $c[$i]->hmedterm_d,
-                        "hlab_d"=>$c[$i]->hlab_d,
-                        "horal_d"=> $c[$i]->horal_d,
-                        "hclass_work_d"=> $c[$i]->hclass_work_d,
-                        "hfinal_d"=> $c[$i]->hfinal_d,
-                        "htotal_d"=> $c[$i]->htotal_d,
-                        "hpass"=> $c[$i]->hpass,
-                        "points"=>$points,
-                        "Degrees"=>$sd,
-                        "Grade Code"=>$code
-                    ] ;
-                   // array_push($aaa,$points,$sd,$code);
-                  // return ($aaa);
-                }
-                else if ($c[$i]->semester == 2)
+                        "Course code=" => $c[$i]->ccode,
+                        "Grade=" => $code,
+                        "Points=" => $points,
+                        "degree" => $sd,
+                        "hpass" => $c[$i]->hpass,
+                        "semester" => $c[$i]->semester,
+                        "year" => $c[$i]->year,
+                        "******" => "******",   ////////////////////////////////////////////////
+                        'hmedterm_d' => $c[$i]->year,
+                        'hlab_d' => $c[$i]->hlab_d,
+                        'horal_d' => $c[$i]->horal_d,
+                        'hclass_work_d' => $c[$i]->hclass_work_d,
+                        'hfinal_d' => $c[$i]->hfinal_d
+                    ];
+
+                if ($c[$i]->semester == 1) {
+                    $aaa[] =
+                        [
+                            "semester" => $c[$i]->semester,
+                            "year" => $c[$i]->year,
+                            "ccode" => $c[$i]->ccode,
+                            "hmedterm_d" => $c[$i]->hmedterm_d,
+                            "hlab_d" => $c[$i]->hlab_d,
+                            "horal_d" => $c[$i]->horal_d,
+                            "hclass_work_d" => $c[$i]->hclass_work_d,
+                            "hfinal_d" => $c[$i]->hfinal_d,
+                            "htotal_d" => $c[$i]->htotal_d,
+                            "hpass" => $c[$i]->hpass,
+                            "points" => $points,
+                            "Degrees" => $sd,
+                            "Grade Code" => $code
+                        ];
+                } else if ($c[$i]->semester == 2) {
+                    $bbb[] =
+                        [
+                            "semester" => $c[$i]->semester,
+                            "year" => $c[$i]->year,
+                            "ccode" => $c[$i]->ccode,
+                            "hmedterm_d" => $c[$i]->hmedterm_d,
+                            "hlab_d" => $c[$i]->hlab_d,
+                            "horal_d" => $c[$i]->horal_d,
+                            "hclass_work_d" => $c[$i]->hclass_work_d,
+                            "hfinal_d" => $c[$i]->hfinal_d,
+                            "htotal_d" => $c[$i]->htotal_d,
+                            "hpass" => $c[$i]->hpass,
+                            "points" => $points,
+                            "Degrees" => $sd,
+                            "Grade Code" => $code
+                        ];
+                } else if ($c[$i]->semester == 3) 
                 {
-                    $bbb[] =  
-                    [
-                        "semester"=>$c[$i]->semester,
-                        "year"=> $c[$i]->year,
-                        "ccode"=>$c[$i]->ccode,
-                        "hmedterm_d"=> $c[$i]->hmedterm_d,
-                        "hlab_d"=>$c[$i]->hlab_d,
-                        "horal_d"=> $c[$i]->horal_d,
-                        "hclass_work_d"=> $c[$i]->hclass_work_d,
-                        "hfinal_d"=> $c[$i]->hfinal_d,
-                        "htotal_d"=> $c[$i]->htotal_d,
-                        "hpass"=> $c[$i]->hpass,
-                        "points"=>$points,
-                        "Degrees"=>$sd,
-                        "Grade Code"=>$code
-                    ] ; ;
+                    $ccc[] =
+                        [
+                            "semester" => $c[$i]->semester,
+                            "year" => $c[$i]->year,
+                            "ccode" => $c[$i]->ccode,
+                            "hmedterm_d" => $c[$i]->hmedterm_d,
+                            "hlab_d" => $c[$i]->hlab_d,
+                            "horal_d" => $c[$i]->horal_d,
+                            "hclass_work_d" => $c[$i]->hclass_work_d,
+                            "hfinal_d" => $c[$i]->hfinal_d,
+                            "htotal_d" => $c[$i]->htotal_d,
+                            "hpass" => $c[$i]->hpass,
+                            "points" => $points,
+                            "Degrees" => $sd,
+                            "Grade Code" => $code
+                        ];
                 }
-                else if ($c[$i]->semester == 3)
-                {
-                    $ccc[] = 
-                     [
-                        "semester"=>$c[$i]->semester,
-                        "year"=> $c[$i]->year,
-                        "ccode"=>$c[$i]->ccode,
-                        "hmedterm_d"=> $c[$i]->hmedterm_d,
-                        "hlab_d"=>$c[$i]->hlab_d,
-                        "horal_d"=> $c[$i]->horal_d,
-                        "hclass_work_d"=> $c[$i]->hclass_work_d,
-                        "hfinal_d"=> $c[$i]->hfinal_d,
-                        "htotal_d"=> $c[$i]->htotal_d,
-                        "hpass"=> $c[$i]->hpass,
-                        "points"=>$points,
-                        "Degrees"=>$sd,
-                        "Grade Code"=>$code
-                    ] ;;
-                }
-            } 
+            }
+            $ddd = [];
+            $cgpa = ["CGPA = " => substr($total_quality / $total_cch, 0, 5)];
+            array_push($ddd, $aaa, $bbb, $ccc, $cgpa);
 
-            $ddd = [] ;
-          $sss= ["CGPA = "=>substr($total_quality/$total_cch,0,5)];
-             array_push($ddd,$aaa,$bbb,$ccc,$sss);
 
-            
 
-           // if($msg=="cgpa") { $form = collect($form)->sortBy('year')->sortBy('semester')->toArray(); }  
+            if ($msg == "cgpa") {
+                return $ddd;
+            }
+
+            if ($msg == "sgpa") // FOR BYAN EL NGAAAAAAAAAAAAAAA7777777
+            {
+                return response()->json(["byan_nga7" => $form, $msg . ":" => substr($total_quality / $total_cch, 0, 5)]);
+            }
+
+            return substr($total_quality / $total_cch, 0, 5);
         }
-       
-
-        if($msg=="cgpa") // FOR BYAN EL NGAAAAAAAAAAAAAAA7777777
-        {
-            return $ddd   ;
-        }
-
-
-        if($msg=="sgpa" ) // FOR BYAN EL NGAAAAAAAAAAAAAAA7777777
-        {
-            return response()->json(["byan_nga7"=>$form,$msg.":"=>substr($total_quality/$total_cch,0,5)])   ;
-        }
-
-        return substr($total_quality/$total_cch,0,5); 
-}
-    }     
+    }
 }
