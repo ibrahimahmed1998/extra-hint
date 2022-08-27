c59db408532d167bc737452b6f573bd2d2022f99
hema <hema.1998.man@gmail.com>
2022-08-27 13:21:39 +0200

    update

--- resources/views/Home/layout.blade.php
+++ resources/views/Home/layout.blade.php
@@ -3,12 +3,12 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
-<script src="{{asset('bjs/bootstrap.min.js')}}" defer></script>
-<link rel="stylesheet" href="{{asset('/css/login.css')}}">
-<link rel="stylesheet" href="{{asset('/css/temp.css')}}">
-<link rel="stylesheet" href="{{asset('bcss/bootstrap.min.css')}}">
+<script src="bjs/bootstrap.min.js" defer></script>
+<link rel="stylesheet" href="/css/login.css">
+<link rel="stylesheet" href="/css/temp.css">
+<link rel="stylesheet" href="bcss/bootstrap.min.css">
 
 <link rel="icon" href="image/Sign-check-icon.png" type="image/icon type">
 
-<link rel="stylesheet" href="{{ asset('css/home.nav.css') }}">
+<link rel="stylesheet" href="css/home.nav.css">
  
