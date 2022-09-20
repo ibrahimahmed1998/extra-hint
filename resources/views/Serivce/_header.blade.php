275e5cb2e35eab7c1c57649e8f372b8698a7d416
hema <hema.1998.man@gmail.com>
2022-09-20 13:56:59 +0200

    Update _header.blade.php

--- resources/views/Serivce/_header.blade.php
+++ resources/views/Serivce/_header.blade.php
@@ -3,6 +3,6 @@
 <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="Sidebar/css/style.css">
-<script src="sidebar/js/moicode.js" ></script>
+<script src="Sidebar/js/moicode.js" ></script>
 
 <meta name="csrf-token" content="{{ csrf_token() }}">
