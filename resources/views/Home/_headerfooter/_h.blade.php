9649661542354a8e76ff7e0b24666421bb7ecf97
hema <hema.1998.man@gmail.com>
2022-09-28 18:06:08 +0200

    Update _h.blade.php

--- resources/views/Home/_headerfooter/_h.blade.php
+++ resources/views/Home/_headerfooter/_h.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="/css/login.css">
     <link rel="stylesheet" href="/css/temp.css">
     <link rel="stylesheet" href="bcss/bootstrap.min.css">
-    <link rel="stylesheet" href="fontawesome/css/all.css">
+    <link rel="stylesheet" href="/fontawesome/css/all.css">
     <link rel="stylesheet" href="css/avatar.css">
     <link rel="stylesheet" href="aos/aos.css">
 
